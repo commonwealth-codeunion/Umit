db860386b857b5cb2ed7fc7303ad2f228936f970
C10ver <c10ver.obj@gmail.com>
2020-05-02 02:11:23 +0600

    Update environment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+
+  firebase: {
+    apiKey: "AIzaSyDzx3ISeGzAkV0buBfbCebr0W54mNLG6f8",
+    authDomain: "umit-9bbcc.firebaseapp.com",
+    databaseURL: "https://umit-9bbcc.firebaseio.com",
+    projectId: "umit-9bbcc",
+    storageBucket: "umit-9bbcc.appspot.com",
+    appId: "1:551342426202:web:69a33398d615a95d8e3063",
+    messagingSenderId: "G-C0JBTW9MHQ"
+  }
 };
